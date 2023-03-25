72ab5b192fdaf856232163c0a533ce9300c671d7
PISERERER <915914042@qq.com>
2023-03-25 21:13:45 +0800

    删除测试文件

--- src/main/java/club/chishazi/headerhelper/GitTestTwo.java
+++
@@ -1,8 +0,0 @@
-package club.chishazi.headerhelper;
-
-public class GitTestTwo {
-    public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println("456");
-    }
-}
